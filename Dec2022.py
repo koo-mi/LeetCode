b45e3bf8b069943260da410a0e25eed95002a94d
Mikael <s.h.koo1213@gmail.com>
2022-12-03 15:20:45 -0500

    2022-12-03

--- Dec2022.py
+++ Dec2022.py
@@ -29,4 +29,41 @@ class Solution:
             li2.append(1)
             li.append(li2)
         return li
+    """
+    Question 121 - Best Time to Buy and Sell Stock
+    2022-12-03
+    """
+    def maxProfit1(self, prices: list[int]) -> int:
+        # First approach but time limit exceeded
+        output = 0
+        for index, value in enumerate(prices):
+            for i in range(index+1, len(prices)):
+                if prices[i] - value > output:
+                    output = prices[i] - value
+        return output
+
+    def maxProfit2(self, prices: list[int]) -> int:
+        # Second approach but time limit exceeded
+        output = 0
+        d_prices = prices.copy()
+        for i in range(len(prices)-1):
+            d_prices.pop(0)
+            val = max(d_prices) - prices[i]
+            if val > output:
+                output = val
+        return output
+
+    def maxProfit3(self, prices: list[int]) -> int:
+        # Third approach
+        left, right = 0, 1  # Buy, Sell
+        output = 0
+        while right < len(prices):
+            if prices[left] < prices[right]:
+                val = prices[right]-prices[left]
+                if output < val:
+                    output = val
+            else:
+                left = right
+            right += 1
+        return output
 
